2bc68e278d944d5c2046cfb414312951304d41f8
Nico Cobelo <ingcobelo@gmail.com>
2020-12-23 09:45:58 -0300

    Finish lecture 1

---
+++ Memorize/memorize/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Nico Cobelo on 21/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack() {
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0)
+                .stroke(lineWidth: 3.0)
+            Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
